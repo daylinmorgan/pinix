90c726d7d35d6e8417ce45c38a720e19dcd9106c
Daylin Morgan <daylinmorgan@gmail.com>
2024-02-13 13:27:15 -0600

    build: split flake into package definition and add overlay

---
+++ default.nix
@@ -0,0 +1,29 @@
+{
+  pkgs,
+  lib,
+  installShellFiles,
+  rustPlatform,
+}:
+rustPlatform.buildRustPackage {
+  pname = "pinix";
+  version = "0.1.0";
+  src = ./.;
+  # nativeBuildInputs = [installShellFiles];
+
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    outputHashes = {
+      "console-0.16.0" = "sha256-t5hydPT3BXJqPl8zKieaId3KUltEbRPh2xmZMhy8Ut0=";
+      "indicatif-0.17.8" = "sha256-8xrGqDb6iUDhdvY937XFqC3GIZpq4tPMZAkKm218c/0=";
+    };
+  };
+
+  meta = with pkgs.lib; {
+    description = "A Pacman inspired frontend for Nix";
+    homepage = "https://github.com/remi-dupre/pinix";
+    license = licenses.lgpl3;
+    mainProgram = "pinix";
+  };
+
+  cargoSha256 = "sha256-6hKbAL3a1t1mHNUvvi65e/BkFoJQmvpZQlU58csmol4=";
+}
--- flake.nix
+++ flake.nix
@@ -1,35 +1,36 @@
 {
-  description = "REST API for any Postgres database";
+  description = "Progress In Nix - Pacman inspired frontend for Nix";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
   };
 
-  outputs = { self, nixpkgs }:
-    with import nixpkgs { system = "x86_64-linux"; };
-
-    pkgs.rustPlatform.buildRustPackage rec {
-      pname = "pinix";
-      version = "0.1.0";
-      src = ./.;
-      nativeBuildInputs = [ pkgs.installShellFiles ];
-
-
-      cargoLock = {
-        lockFile = ./Cargo.lock;
-        outputHashes = {
-          "console-0.16.0" = "sha256-t5hydPT3BXJqPl8zKieaId3KUltEbRPh2xmZMhy8Ut0=";
-          "indicatif-0.17.8" = "sha256-8xrGqDb6iUDhdvY937XFqC3GIZpq4tPMZAkKm218c/0=";
-        };
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+  }: let
+    inherit (nixpkgs.lib) genAttrs;
+    eachSystem = f:
+      genAttrs (import systems)
+      (system:
+        f system
+        (import nixpkgs {
+          inherit system;
+          overlays = [self.overlays.default];
+        }));
+  in {
+    overlays = {
+      default = final: _prev: {
+        pinix = final.callPackage ./default.nix {};
       };
-
-      meta = with pkgs.lib; {
-        description = "A Pacman inspired frontend for Nix";
-        homepage = "https://github.com/remi-dupre/pinix";
-        license = licenses.lgpl3;
-        mainProgram = "pinix";
-      };
-
-      cargoSha256 = "sha256-6hKbAL3a1t1mHNUvvi65e/BkFoJQmvpZQlU58csmol4=";
     };
+    packages = eachSystem (system: (pkgs: {
+      pinix = pkgs.pinix;
+      default = self.packages.${pkgs.system}.pinix;
+    }));
+
+      };
+  };
 }
